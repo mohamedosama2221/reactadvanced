9b09cb281c903f59b11dc7e578f6743ebdf981e9
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-09-29 17:44:39 +0400

    useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+// you can use multiple useEffects in the same component
+//use effect replace both componentdidmount and componentdidupdate
+//use effect with empty []  replace  componentdidmount only as it runs once
+//use effect with [value]  replace both componentdidmount and componentdidupdate (but only each time this "value" in the array changes)
 const UseEffectBasics = () => {
+  const [value , setValue] = useState(0)
   useEffect(()=>{
-    
+    if(value > 0){
+
+      document.title = `${value} new messages`
+    }
   })
-  const [value , setValue] = useState('one')
   const changeValue = ()=>{
     setValue((prevState)=>{
-      return "three"
+      return(prevState+1)
     })
   }
   return (
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,18 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size , setSize] = useState(window.innerWidth);
+  const setsize = ()=>{
+    setSize(window.innerWidth)
+  }
+  useEffect(()=>{
+
+    window.addEventListener('resize' ,setsize );
+    return ()=>{
+    window.removeEventListener('resize' ,setsize );
+    }
+  })
+  return <h2>{size}</h2>;
 };
 
 export default UseEffectCleanup;
