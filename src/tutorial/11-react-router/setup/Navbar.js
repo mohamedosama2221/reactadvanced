9663ebca97a0e9698dd061c194c88e33c51e5db0
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-10-03 14:30:52 +0400

    react-route

--- src/tutorial/11-react-router/setup/Navbar.js
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/people">People</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const { id } = useParams();
+  const [name, setName] = useState();
+  useEffect(() => {
+    const onePerson = data.find((p) => p.id === parseInt(id));
+
+    setName(onePerson.name);
+  }, [id]);
   return (
-    <div>
-      <h2>person</h2>
+    <div className="item">
+      <h4>{name}</h4>
+      <Link to="/people" className="btn">
+        get back
+      </Link>
     </div>
   );
 };
